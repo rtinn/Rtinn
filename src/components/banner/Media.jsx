c2d2926207a794b646a5cc21d3d0351578c8c606
rtinn <135967326+rtinn@users.noreply.github.com>
2025-05-26 17:12:00 +0300

    rtinn
    rtinn

--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact, FaGithub } from "react-icons/fa";
-import { SiTailwindcss, SiFigma, SiNextdotjs, SiJava, SiPhp } from "react-icons/si";
+import { FaFacebookF, FaLinkedinIn, FaReact, FaGithub } from "react-icons/fa";
+import { SiTailwindcss, SiJava, SiPhp } from "react-icons/si";
 const Media = () => {
   return (
     <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
